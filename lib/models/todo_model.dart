42894f8938836b3f04c29889eb5265d140538053
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-10 17:29:59 +0500

    imp

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -1,5 +1,15 @@
 class TodoModel {
   // members
+  late int userId;
+  late int id;
+  late String title;
+  late bool completed;
 
   // fromJson
+  TodoModel.fromJson(Map json){
+    userId = json["userId"];
+    id = json["id"];
+    title = json["title"];
+    completed = json["completed"];
+  }
 }
